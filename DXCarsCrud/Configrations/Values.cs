001c24b461a69e39dd86198a53a5f1e078468bc0
elmagek mosaad <elmagekmosaad3535@gmail.com>
2024-10-24 01:04:34 +0300

    Added Weather Service By City Name From Api

---
+++ DXCarsCrud/Configrations/Values.cs
@@ -0,0 +1,8 @@
+﻿namespace DXCarsCrud.Configrations
+{
+    public class Values
+    {
+        //https://api.weatherapi.com/v1/current.json?key=f026e8cc155a4682bf9145047242310&q=cairo
+        public const string apiUrl = "https://api.weatherapi.com/v1/current.json?key=f026e8cc155a4682bf9145047242310&";
+    }
+}
---
+++ DXCarsCrud/Controllers/WeatherController.cs
@@ -0,0 +1,40 @@
+﻿using DXCarsCrud.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+
+namespace DXCarsCrud.Controllers
+{
+    public class WeatherController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task<IActionResult> SearchByCityAsync(string? city)
+        {
+            if (city == null)
+            {
+                city = "cairo";
+            }
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                //GET Method
+                HttpResponseMessage response = await client.GetAsync(Configrations.Values.apiUrl + "q=" + city);
+                if (response.IsSuccessStatusCode)
+                {
+                    WeatherResponse WeatherResponse = await response.Content.ReadFromJsonAsync<WeatherResponse>();
+
+                    return View(WeatherResponse);
+                }
+                else
+                {
+                    return BadRequest(response.StatusCode);
+                }
+            }
+
+            return View();
+        }
+    }
+}
